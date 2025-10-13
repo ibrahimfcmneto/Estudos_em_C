d6a85c5e854fe690fe64e169d24e824bb1f5cb44
ibrahimfcmneto <ibrahim.fcmn@puccampinas.edu.br>
2025-10-13 11:37:10 -0300

    comparacao_endereco_variavel

---
+++ comando_basicos/comparacao_endereco_variavel.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int variavel1 = 10;
+    int variavel2 = 11;
+
+    int *endereco1 = &variavel1;
+    int *endereco2 = &variavel2;
+
+    printf("Endereço da variavel1: %p\n", (void*)endereco1);
+    printf("Endereço da variavel2: %p\n", (void*)endereco2);
+
+    int maior_endereco = endereco1;
+
+    if (endereco2 > endereco1)
+    {
+        maior_endereco = endereco2;
+    }
+
+    printf("O maior endereo eh: %p", (void*)maior_endereco);
+
+
+    return 0;
+}
