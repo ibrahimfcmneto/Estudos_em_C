30166e9e4edcc08cd81a7c1dda0878183ee3c8f3
ibrahimfcmneto <ibrahim.fcmn@puccampinas.edu.br>
2025-11-11 15:39:34 -0300

    teoria de lista

---
+++ estrutura_de_dados/lista/teoria.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//é armazenado um dado e um endereço e os elementos vão sendo lincados
+//quando quer adicionar um elemento no meio precisa mover todo que estão na frente
+//e colocar o elemento no certo, para remover a mesma coisa, remove e volta uma casa todos
+
+
+//precisa alocar memoria
+//int* inteiro = (int*)malloc(sizeof(int));
